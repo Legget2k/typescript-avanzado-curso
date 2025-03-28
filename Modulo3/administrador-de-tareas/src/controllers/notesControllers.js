ae23b67b0b4e66d1437ef5f021922284199ffbfd
leggetram <leggetram@gmail.com>
2025-03-28 08:21:17 -0300

    Elimina node_modules del repositorio

--- Modulo3/administrador-de-tareas/src/controllers/notesControllers.js
+++ Modulo3/administrador-de-tareas/src/controllers/notesControllers.js
@@ -1,4 +1,5 @@
 // recibir y responder al usuario
+import  Mongoose  from 'mongoose';
 import { Note } from '../models/noteModel.js';
 
 export const createNotes = async (req, res) => {
@@ -13,3 +14,50 @@ export const createNotes = async (req, res) => {
         next(error);
     }
 };
+
+export const getNotes = async (req, res, next) => {try {
+    const notes = await Note.find();
+    notes.json(notes);
+} catch (error) {
+    next(error);
+}}
+
+export const getNotesById = async (req, res, next) => {try {
+    const { id } = req.params;
+    const note = await Note.findById(id);
+    if (!note) {
+        const error = new Error(`Note with id ${id} not found`);
+        error.code = 404;
+        throw error;
+    }
+    res.json(note);
+} catch (error) {
+    next(error);
+}}
+
+export const updateNotes = async (req, res, next) => {try {
+    const { id } = req.params;
+    const { body } = req;
+
+    if(!Mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({message: "Invalid ObjectId format"});
+    }
+
+    const updatedNote = await Note.findByIdAndUpdate(id, body, {new: true});
+    if (!updatedNote) { res.status(404).json({message: `Note not found`}); }
+    res.json(updatedNote);
+
+} catch (error) {
+    next(error);
+}}
+
+export const deleteNotes = async (req, res, next) => {try {
+    const { id } = req.params;
+    if(!Mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({message: "Invalid ObjectId format"});
+    }
+    const deletedNote = await Note.findByIdAndDelete(id);
+    if (!deletedNote) { res.status(404).json({message: `Note not found`}); }
+} catch (error) {
+    next(error);
+}}
--- Modulo3/administrador-de-tareas/src/routes/notesRoutes.js
+++ Modulo3/administrador-de-tareas/src/routes/notesRoutes.js
@@ -4,9 +4,14 @@
 //PUT /notes/:id
 //DELETE /notes/:id
 import { Router } from "express";
-import { createNotes } from "../controllers/notesControllers.js";
+import { createNotes, getNotes, getNotesById, updateNotes, deleteNotes } from "../controllers/notesControllers.js";
 
 const NotesRouter = Router();
+
+NotesRouter.get("/", getNotes);
+NotesRouter.get("/:id", getNotesById);
+NotesRouter.patch("/:id", updateNotes); // put -- patch -> put: actualiza todo el objeto, patch: actualiza solo una parte del objeto
+NotesRouter.post("/:id", deleteNotes);
 
 NotesRouter.post("/", createNotes);
 
